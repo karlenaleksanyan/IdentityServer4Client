f010ece9f15c2336ec6ce9b1591470bdcc2d8c85
karlen.aleksanyan.1999 <karlen.aleksanyan.1999@gmail.com>
2021-04-18 23:21:45 +0400

    Add project files.

---
+++ WeatherMVC/Services/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherMVC.Services
+{
+	public interface ITokenService
+	{
+		public Task<TokenResponse> GetToken(string scope);
+	}
+}
